06ff487a323b770f91559a9cc2629f943466ab0a
Kasper <kasper.mustad.juell@linux.com>
2024-05-31 16:59:29 +0200

    Moved run tui to seperate file

--- cmd/main.go
+++ cmd/main.go
@@ -1,61 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/kmuju/TuiCalendar/cmd/tui"
 )
 
-type model struct {
-	cal tui.Calendar
-}
-
-func initialModel() model {
-	events := tui.CreateEvents()
-	return model{
-		tui.NewCalendar(events, 20, 100, 40, 0, 5),
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "k", "up":
-			m.cal.Up()
-		case "j", "down":
-			m.cal.Down()
-			// case "h", "left":
-			// 	m.week.Left()
-			// case "l", "right":
-			// 	m.week.Right()
-		}
-	}
-	return m, nil
-}
-
-func (m model) View() string {
-	return m.cal.Render()
-}
-
 func main() {
-	f, err := tea.LogToFile("debug.log", "debug")
-	if err != nil {
-		fmt.Println("fatal:", err)
-		os.Exit(1)
-	}
-	defer f.Close()
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
+	tui.Run()
 }
---
+++ cmd/tui/run.go
@@ -0,0 +1,60 @@
+package tui
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type teaModel struct {
+	cal Calendar
+}
+
+func initialModel() teaModel {
+	events := CreateEvents()
+	return teaModel{
+		NewCalendar(events, 20, 100, 40, 0, 5),
+	}
+}
+
+func (m teaModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m teaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "k", "up":
+			m.cal.Up()
+		case "j", "down":
+			m.cal.Down()
+			// case "h", "left":
+			// 	m.week.Left()
+			// case "l", "right":
+			// 	m.week.Right()
+		}
+	}
+	return m, nil
+}
+
+func (m teaModel) View() string {
+	return m.cal.Render()
+}
+
+func Run() error {
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		fmt.Println("fatal:", err)
+		return err
+	}
+	defer f.Close()
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		return err
+	}
+	return nil
+}
