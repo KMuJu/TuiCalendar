e4dd5e38c078bec57a2228c2038c0f833238dee5
Kasper <kasper.mustad.juell@linux.com>
2024-05-31 16:54:52 +0200

    Move event to seperate module

---
+++ cmd/model/event.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+)
+
+type Event struct {
+	Id          string
+	Date        int
+	Name        string
+	Description string
+	Start       time.Time
+	End         time.Time
+}
+
+func (e Event) Equal(other Event) bool {
+	return e.Id == other.Id &&
+		e.Name == other.Name
+}
