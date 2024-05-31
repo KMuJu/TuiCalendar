30e1aac6a8f6d642a9a7dd3bff411f9e1c39ff16
Kasper <kasper.mustad.juell@linux.com>
2024-05-31 20:06:59 +0200

    Add cli functionality --sync will update database

--- main.go
+++ main.go
@@ -1,38 +1,64 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"slices"
 
-	d "github.com/kmuju/TuiCalendar/cmd/db"
+	"github.com/kmuju/TuiCalendar/cmd/db"
+	"github.com/kmuju/TuiCalendar/cmd/google"
 	"github.com/kmuju/TuiCalendar/cmd/model"
 	"github.com/kmuju/TuiCalendar/cmd/tui"
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	db, err := d.InitDB()
+	cmd := &cli.Command{
+		Name: "TuiCalendar",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{Name: "sync"},
+		},
+		Action: func(ctx context.Context, c *cli.Command) error {
+			if c.Bool("sync") {
+				// _, err := exec.Command("./sync").Output()
+				// return err
+				update()
+				return nil
+			}
+			con, err := db.InitDB()
+			if err != nil {
+				return nil
+			}
+			events, err := db.GetEvents(con)
+			if err != nil {
+				return nil
+			}
+			slices.SortFunc(events, func(a, b model.Event) int {
+				if a.Start.Before(b.Start) {
+					return -1
+				}
+				if a.Start.After(b.Start) {
+					return 1
+				}
+				return 0
+			})
+			tui.Run(events)
+			return nil
+		},
+	}
+
+	cmd.Run(context.Background(), os.Args)
+}
+
+func update() {
+
+	con, err := db.InitDB()
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	e := model.Event{
-		Name:        "Test",
-		Description: "ALS",
-	}
-	d.InsertEvent(db, e)
-
-	dbevents, err := d.GetEvents(db)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, ev := range dbevents {
-		fmt.Printf("%v\n", ev)
-	}
-
-	events := tui.CreateEvents()
-	d.Sync(db, events)
-
-	dbevents, err = d.GetEvents(db)
+	google.Update(con)
+	dbevents, err := db.GetEvents(con)
 	if err != nil {
 		fmt.Println(err)
 		return
